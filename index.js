1f753f71a8d2a524d05e07c6e8b74ed3867dc9ae
chengying <hollye2558@gmail.com>
2022-11-19 13:14:21 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+ // const { bodyParser } = require("json-server");
+
 // Add your code here
+
+
+function submitData(name,email) {
+    const userInfo = {
+        name: name,
+        email: email
+    }
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userInfo)
+
+    })
+    .then(res => res.json())
+    .then(data => {
+       const li = document.createElement("li");
+       li.textContent = data.id
+       document.querySelector("body").append(li);
+
+    } )
+    .catch(error => {
+        const p = document.createElement("p");
+        p.textContent = error;
+        document.querySelector("body").append(p)
+        
+    })
+}
